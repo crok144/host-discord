6af49755a490f86ec83bd6bb8b79201a780b9d7c
crok144 <crok27142@gmail.com>
2026-03-26 22:57:51 +0300

    Update X2.py

--- X2.py
+++ X2.py
@@ -7,25 +7,15 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+@bot.event 
+async def on_ready():
+    print("А вот и я!")
+
 @bot.event
 async def on_member_join(member):
     channel_id = 1486288743454474280 
-    try:
-        channel = await bot.fetch_channel(channel_id)
-        await channel.send(f"Добро пожаловать в мой мир, {member.mention}! Рады тебя видеть!")
-        print(f"Сообщение успешно отправлено в канал {channel_id}")
-    except Exception as e:
-        print(f"ОШИБКА ПРИВЕТСТВИЯ: {e}")
+    channel = bot.get_channel(channel_id)
 
-    role_id = 148283396685762673 
-    role = member.guild.get_role(role_id)
-    
-    if role:
-        try:
-            await member.add_roles(role)
-            print(f"Роль успешно выдана: {member.name}")
-        except Exception as e:
-            print(f"Не удалось выдать роль: {e}")
-    else:
-        print(f"Роль с ID {role_id} не найдена на сервере!")
+    if channel:
+        await channel.send(f"Добро пожаловать в мой мир, {member.mention}! Рады тебя видеть <:Xhi:1486275957370261645>")
 bot.run(os.getenv('BOT_TOKEN'))
