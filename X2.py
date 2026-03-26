764b0422b460895429aa497b3a0c3ba3bfc19c3a
crok144 <crok27142@gmail.com>
2026-03-26 21:36:42 +0300

    Update X2.py

--- X2.py
+++ X2.py
@@ -23,4 +23,4 @@ async def on_member_join(member):
     if role:
         await member.add_roles(role)
         print(f"Роль выдана пользователю {member.name}")
-bot.run("MTQ4NjQzNDA4MTkwOTY0MTIzNg.GEeijD.u9N6xfZqadKuBlXUldT7wIOEkIevTEL5oKqotM")
+bot.run("MTQ4NjQzNDA4MTkwOTY0MTIzNg.Gyql01.l-ap86ndyCx8SUlKrwFWgGGb3APhGDwFhNRtsQ")
