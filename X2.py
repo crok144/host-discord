57e98fba978e27e3d1f7b25a6f8c06367c4a1408
crok144 <crok27142@gmail.com>
2026-03-26 23:28:35 +0300

    Update X2.py

--- X2.py
+++ X2.py
@@ -3,26 +3,26 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.members = True 
+intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event 
+@bot.event
 async def on_ready():
     print("А вот и я!")
 
 @bot.event
 async def on_member_join(member):
-    channel_id = 1486288743454474280 
+    channel_id = 1486288743454474280
     channel = bot.get_channel(channel_id)
-
     if channel:
-        await channel.send(f"Добро пожаловать в мой мир, {member.mention}! Рады тебя видеть <:Xhi:1486275957370261645>")
-@bot.event
-async def on_member_join(member):
-    role = member.guild.get_role(1486283396685762673)
+        await channel.send(f"Добро пожаловать в мой мир, {member.mention}! Рады тебя видеть!")
+
+    role_id = 1486283396685762673
+    role = member.guild.get_role(role_id)
     if role:
         await member.add_roles(role)
         print(f"Роль выдана пользователю {member.name}")
-import os
+
 bot.run(os.getenv('BOT_TOKEN'))
