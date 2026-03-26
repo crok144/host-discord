f3acf8069cbecf5c154278b8d360404ba22f37d0
crok144 <crok27142@gmail.com>
2026-03-26 23:07:49 +0300

    Update X2.py

--- X2.py
+++ X2.py
@@ -18,4 +18,10 @@ async def on_member_join(member):
 
     if channel:
         await channel.send(f"Добро пожаловать в мой мир, {member.mention}! Рады тебя видеть <:Xhi:1486275957370261645>")
+@bot.event
+async def on_member_join(member):
+    role = member.guild.get_role(1486283396685762673)
+    if role:
+        await member.add_roles(role)
+        print(f"Роль выдана пользователю {member.name}")
 bot.run(os.getenv('BOT_TOKEN'))
