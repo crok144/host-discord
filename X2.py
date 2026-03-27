7d389009404fc6059e3e62c960cee933ff885083
crok144 <crok27142@gmail.com>
2026-03-27 19:45:33 +0300

    Update X2.py

--- X2.py
+++ X2.py
@@ -13,14 +13,20 @@ processed_members = set()
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Sigma"))
-    print(f"Бот запущен как {bot.user}")
+    print(f"А вот и я! Запущен как {bot.user}")
+
+@bot.event
+async def on_member_remove(member):
+    channel_id = 1486995771365523568
+    channel = bot.get_channel(channel_id)
+    if channel:
+        await channel.send(f"{member.name}, ты меня УТОМЛЯЕШЬ!")
+        print(f"Пользователь {member.name} вышел с сервера.")
 
 @bot.event
 async def on_member_join(member):
     if member.id in processed_members:
-        print(f"Повторный вход {member.name} проигнорирован.")
         return
-
     processed_members.add(member.id)
 
     channel_id = 1486288743454474280
@@ -29,13 +35,11 @@ async def on_member_join(member):
         await channel.send(f"Добро пожаловать в мой мир, {member.mention}! Рады тебя видеть!")
     
     try:
-        role_id = 1486283396857626731 
+        role_id = 148628339685762673 
         role = member.guild.get_role(role_id)
         if role:
             await member.add_roles(role)
             print(f"Роль выдана пользователю {member.name}")
-        else:
-            print(f"Роль с ID {role_id} не найдена.")
     except Exception as e:
         print(f"Ошибка при выдаче роли: {e}")
 
