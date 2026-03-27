5877c8d730f94ecca25227575a98ae92298fa8f1
crok144 <crok27142@gmail.com>
2026-03-27 18:28:43 +0300

    Update X2.py

--- X2.py
+++ X2.py
@@ -13,6 +13,15 @@ async def on_ready():
     print("А вот и я!")
 
 @bot.event
+async def on_member_remove(member):
+    channel_id = 1486995771365523568
+    channel = bot.get_channel(channel_id)
+
+    if channel:
+        await channel.send(f"{member.name}, ты меня УТОМЛЯЕШЬ!")
+        print(f"Пользователь {member.name} вышел с сервера.")
+
+@bot.event
 async def on_member_join(member):
     channel_id = 1486288743454474280
     channel = bot.get_channel(channel_id)
